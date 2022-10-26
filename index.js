866159556962e08769b3c104955d1fc8651b878a
devsrabon <et.srabon@gmail.com>
2022-10-26 21:25:51 +0600

    added courses card changes

--- index.js
+++ index.js
@@ -5,24 +5,22 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
+const courses = require('./data/categories.json');
 
-const course = require('./data/course.json');
+const courseDetail = require('./data/course.json');
 
 app.get('/', (req, res) => {
 	res.send('Programming API is running');
 });
 
-app.get('/categories', (req, res) => {
-	res.send(categories);
+app.get('/courses', (req, res) => {
+	res.send(courses);
 });
 
-
-
-app.get('/course/details/:id', (req, res) => {
+app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     console.log(id)
-    const selectedCourse = course.find((p) => p.id == id);
+    const selectedCourse = courseDetail.find((p) => p.id == id);
 	res.send(selectedCourse);
     console.log(selectedCourse);
 });
