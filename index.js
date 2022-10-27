78cf8734a3f644f734dbc67e4f601a610b834f39
devsrabon <et.srabon@gmail.com>
2022-10-27 18:16:33 +0600

    added courses details

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ app.get('/courses', (req, res) => {
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     console.log(id)
+    const selectedCourse = courses.find((p) => p.id == id);
+	res.send(selectedCourse);
+    console.log(selectedCourse);
+});
+app.get('/courses/details/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
     const selectedCourse = courseDetail.find((p) => p.id == id);
 	res.send(selectedCourse);
     console.log(selectedCourse);
